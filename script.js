83f31c82504deda07d172efea65e66c3f82a6b8e
Kartik Arora <chipset95@yahoo.co.in>
2018-05-25 12:35:46 +1000

    initial something 2

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-var x = [],y=[];
 var color = [];
-var  song, fft;
+var song, fft;
+var playing = false;
 
-function preload(){
+function preload() {
     song = loadSound('song3.mp3');
 }
 
 function setup() {
+    // createCanvas(1024, 720);
     createCanvas(windowWidth, windowHeight);
     soundFormats('mp3');
     color = [random(0, 255), random(0, 255), random(0, 255)];
     song.setVolume(1);
     song.play();
+    playing = true;
     fft = new p5.FFT();
     fft.setInput(song);
 }
@@ -19,8 +21,26 @@ function setup() {
 function draw() {
     background(color);
     var spec = fft.analyze();
-    for (i = 0; i<spec.length; i++) {
-        fill(spec[i]);
-        ellipse(map(i,0,spec.length,0,width),map(spec[i], 0, 255, height,0),10);
+    for (i = 0; i < spec.length; i++) {
+        stroke(spec[i]);
+        line(map(i, 0, spec.length, 0, width), 0, map(i, 0, spec.length, 0, width), map(spec[i], 0, 255, height, 0));
     }
+    fill(255);
+    textSize(56);
+    if (song.isPlaying()) {
+        text("Playing", width / 2 - 30, 50);
+    } else {
+        text("Paused", width / 2 - 30, 50);
+    }
+}
+
+function keyPressed() {
+    if (keyCode === 32) {
+        if (song.isPlaying()) {
+            song.pause(0);
+        } else {
+            song.play();
+        }
+    }
+    return false; // prevent default
 }
