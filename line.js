ee580d6160be289f8236c90706af5ccf1f261180
Kartik Arora <chipset95@yahoo.co.in>
2018-06-02 19:52:29 +1000

    major iteration 1.3

--- line.js
+++ line.js
@@ -5,14 +5,16 @@ class Line {
         this._spectrum = spectrum;
     }
 
-    draw() {
-        push();
-        translate(0, this._height);
-        noStroke();
-        fill(this._color);
+    draw(graphics) {
+        graphics.push();
+        graphics.translate(0, this._height);
+        graphics.noStroke();
+        graphics.fill(this._color);
         for (var i = 0; i < this._spectrum.length; i++) {
-            ellipse(map(i, 0, this._spectrum.length, 256, width - 256), map(this._spectrum[i], 0, 255, 64, 0), map(this._spectrum[i], 0, 255, 1, 10));
+            graphics.ellipse(map(i, 0, this._spectrum.length, 256, graphics.width - 256),
+                map(this._spectrum[i], 0, 255, 64, 0),
+                map(this._spectrum[i], 0, 255, 1, 5));
         }
-        pop();
+        graphics.pop();
     }
 }
--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-var song, fft, to, spectrum, canvas, font;
-var r, loaded = false;
+var song, fft, to, spectrum, canvas, bg, tg;
+var r, loaded = false, save, spotify;
 var lines = [];
 var song_name = ['Heavy Metal', 'Canon', 'Bohemian Rhapsody', 'Radio Ga Ga'];
 var song_artist = ['Justice', 'Justice', 'The Queen', 'The Queen'];
+var song_url = ['https://open.spotify.com/track/621W0YFtYurfPC6hwvmWgc?si=ihsnFk3bSzeIKPWlB9J7Cg',
+    'https://open.spotify.com/track/5GsdNmkpAbtZnUt5VxtbHm?si=B3gu3WrcSNmmCA64eOTIOQ',
+    'https://open.spotify.com/track/1ONqVFerUEyoXpgtBpbzfM?si=P56G95CKSmel-5l1rCpz2w',
+    'https://open.spotify.com/track/30cjrAb2I858LOMhwGcrjd?si=6fUurRWaSruNQ1ueE0lBIQ'];
 
 function loadingComplete() {
     song.setLoop(false);
@@ -12,30 +16,39 @@ function loadingComplete() {
     fft.setInput(song);
     to = color(0, 0, 0);
     loaded = true;
+    save.position(windowWidth / 2 - 100 - 24, windowHeight - 90);
+    spotify.position(windowWidth / 2 + 100 - 24, windowHeight - 90);
+    save.hide();
+    spotify.hide();
+    save.mousePressed(saveArt);
+    spotify.mousePressed(openSpotify);
 }
 
 function setup() {
-    canvas = createCanvas(856, 856);
+    canvas = createCanvas(856, 956);
+    bg = createGraphics(856, 100);
+    tg = createGraphics(856, 856);
     centerCanvas();
     r = random([1, 2, 3, 4]);
-    font = loadFont('font.ttf');
+    save = createImg(r > 2 ? 'save_dark.svg' : 'save_light.svg');
+    spotify = createImg(r > 2 ? 'spotify_dark.svg' : 'spotify_light.svg');
     song = loadSound('song' + r + '.mp3', loadingComplete);
     soundFormats('mp3');
-
 }
 
 function draw() {
     if (loaded) {
+        //Top Graphics
         spectrum = fft.analyze().splice(0, 640);
-        background(r > 2 ? 0 : 255);
+        tg.background(r > 2 ? 0 : 255);
 
         // borders
-        stroke(r > 2 ? 255 : 0);
-        strokeWeight(2);
-        line(64, 64, map(song.currentTime(), 0, song.duration(), 64, width - 64), 64);
-        line(64, 64, 64, map(song.currentTime(), 0, song.duration(), 64, height - 64));
-        line(width - 64, height - 64, width - 64, map(song.currentTime(), 0, song.duration(), height - 64, 64));
-        line(width - 64, height - 64, map(song.currentTime(), 0, song.duration(), width - 64, 64), height - 64);
+        tg.stroke(r > 2 ? 255 : 0);
+        tg.strokeWeight(2);
+        tg.line(64, 64, map(song.currentTime(), 0, song.duration(), 64, tg.width - 64), 64);
+        tg.line(64, 64, 64, map(song.currentTime(), 0, song.duration(), 64, tg.height - 64));
+        tg.line(tg.width - 64, tg.height - 64, tg.width - 64, map(song.currentTime(), 0, song.duration(), tg.height - 64, 64));
+        tg.line(tg.width - 64, tg.height - 64, map(song.currentTime(), 0, song.duration(), tg.width - 64, 64), tg.height - 64);
 
         // spectral lines
         while (lines.length > 0) {
@@ -43,57 +56,67 @@ function draw() {
         }
         for (var i = 0; i < 5; i++) {
             lines.push(new Line(
-                (i * (height - 512) / 6) + 256,
+                (i * (tg.height - 512) / 6) + 256,
                 color(r > 2 ? 255 : 0),
                 splitSpectrum(spectrum, 128)[i]
             ));
         }
 
         lines.forEach(function (line) {
-            line.draw();
+            line.draw(tg);
         });
 
         // text
-        fill(r > 2 ? 255 : 0);
-        textSize(32);
-        // textFont(font);
-        // textStyle(ITALIC);
-        push();
-        translate(width / 2, height / 2);
-        rotate(-PI / 2);
-        translate(-width / 2, -height / 2);
-        text(song_artist[r - 1], 32, 44);
-        pop();
-        push();
-        translate(width / 2, height / 2);
-        rotate(PI / 2);
-        translate(-width / 2, -height / 2);
-        text(song_name[r - 1], 32, 44);
-        pop();
+        tg.fill(r > 2 ? 255 : 0);
+        tg.textSize(32);
+
+        tg.push();
+        tg.translate(tg.width / 2, tg.height / 2);
+        tg.rotate(-PI / 2);
+        tg.translate(-tg.width / 2, -tg.height / 2);
+        tg.text(song_artist[r - 1], 32, 44);
+        tg.pop();
+
+        tg.push();
+        tg.translate(tg.width / 2, tg.height / 2);
+        tg.rotate(PI / 2);
+        tg.translate(-tg.width / 2, -tg.height / 2);
+        tg.text(song_name[r - 1], 32, 44);
+        tg.pop();
+
+        // Bottom Graphics
+        bg.background(r > 2 ? 255 : 0);
+        spotify.show();
+        save.show();
+
+        // draw canvas
+        image(tg, 0, 0);
+        image(bg, 0, 857);
 
     } else {
         background(r > 2 ? 0 : 255);
         fill(r > 2 ? 255 : 0);
         push();
         translate(width / 2, height / 2);
-        ellipse(0, map(sin(frameCount / 10 % PI), -1, 1, 50, -50), 50);
+        ellipse(0, map(sin(frameCount / 10 % PI), -1, 1, 100, -100), 50);
     }
 }
 
 function keyPressed() {
     if (keyCode === 32) {
-        mousePressed();
+        pauseToggle();
     }
     return false;
 }
 
-function mousePressed() {
+function pauseToggle() {
     if (song.isPlaying()) {
         song.pause();
     } else {
         song.play();
     }
 }
+
 
 function centerCanvas() {
     var x = (windowWidth - width) / 2;
@@ -102,7 +125,11 @@ function centerCanvas() {
 }
 
 function saveArt() {
-    saveCanvas('album_art', 'png');
+    saveCanvas(tg, 'album_art', 'png');
+}
+
+function openSpotify() {
+    window.open(song_url[r - 1], '_blank');
 }
 
 function splitSpectrum(arr, chunkSize) {
